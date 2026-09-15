6941923530ecebd89d5f7559befaf97c285c9239
Jack <jpb0042@mix.wvu.edu>
2026-09-15 16:12:13 -0400

    fix

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,7 +11,7 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   {
-    ignores: ['.next/**', 'dist/**', 'out/**', 'node_modules/**'],
+    ignores: ['.next/**', 'dist/**', 'out/**', 'node_modules/**', 'next-env.d.ts'],
   },
   ...compat.extends('next/core-web-vitals', 'next/typescript'),
   {
